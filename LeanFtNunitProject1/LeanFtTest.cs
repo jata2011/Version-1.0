5ad7a8e678670a87841ca79323ac99d8c219f205
jata2011 <jata2011@gmail.com>
2017-04-25 17:06:21 +1000

    Version 1.0
    ver 1.0

---
+++ LeanFtNunitProject1/LeanFtTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+using HP.LFT.SDK;
+using HP.LFT.Verifications;
+using HP.LFT.SDK.Web;
+
+namespace LeanFtNunitProject1
+{
+    [TestFixture]
+    public class LeanFtTest : UnitTestClassBase
+    {
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            // Setup once per fixture
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Before each test
+            
+        }
+
+        [Test]
+        public void Test()
+        {
+            IBrowser browser = BrowserFactory.Launch(BrowserType.Chrome);
+            browser.Navigate("https://saas.hp.com/software/leanft");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            // Clean up after each test
+        }
+
+        [TestFixtureTearDown]
+        public void TestFixtureTearDown()
+        {
+            // Clean up once per fixture
+        }
+    }
+}
